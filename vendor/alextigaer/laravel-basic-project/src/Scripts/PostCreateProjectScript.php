7a8fab3d444073d26c1eb415478a3f77656fe76b
christoperw818 <christoperw818@gmail.com>
2017-12-19 19:46:10 +0100

    Added script to execute after project creation

---
+++ vendor/alextigaer/laravel-basic-project/src/Scripts/PostCreateProjectScript.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace LaravelBasicProject\Scripts;
+
+class PostCreateProjectScript
+{
+
+    public function editEnv(){
+        echo "PROVA";
+        echo config('app.name');
+    }
+
+}
