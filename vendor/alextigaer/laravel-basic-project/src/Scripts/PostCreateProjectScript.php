5bf520962c0af2679751f93d9e23ed9ef07a3400
christoperw818 <christoperw818@gmail.com>
2017-12-19 23:03:08 +0100

    Edited PostCreateProjectScript.php

--- vendor/alextigaer/laravel-basic-project/src/Scripts/PostCreateProjectScript.php
+++ vendor/alextigaer/laravel-basic-project/src/Scripts/PostCreateProjectScript.php
@@ -10,6 +10,11 @@ class PostCreateProjectScript
     public static function editEnv(Event $event){
         echo "\n\n";
         print_r($event);
+
+        $vendorDir = $event->getComposer()->getConfig()->get('vendor-dir');
+
+        echo "\n\n".$vendorDir;
+
     }
 
 }
