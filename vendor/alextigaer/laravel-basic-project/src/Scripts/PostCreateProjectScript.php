cb45b97a371d4021e9e3f00ae1c25581479596b7
Tiger <alvietri@gmail.com>
2017-12-19 22:59:09 +0100

    Edited PostCreateProjectScript.php

--- vendor/alextigaer/laravel-basic-project/src/Scripts/PostCreateProjectScript.php
+++ vendor/alextigaer/laravel-basic-project/src/Scripts/PostCreateProjectScript.php
@@ -8,8 +8,8 @@ class PostCreateProjectScript
 {
 
     public static function editEnv(Event $event){
-        echo "\n\n".$event->getName()."\n\n";
-        print_r($event->getArguments());
+        echo "\n\n";
+        print_r($event);
     }
 
 }
