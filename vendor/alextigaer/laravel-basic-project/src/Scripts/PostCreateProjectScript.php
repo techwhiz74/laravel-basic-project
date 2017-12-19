d1d42aa0dea95f1fe3a3dd1cf175a08ed420f924
Tiger <alvietri@gmail.com>
2017-12-19 22:55:23 +0100

    Edited PostCreateProjectScript.php

--- vendor/alextigaer/laravel-basic-project/src/Scripts/PostCreateProjectScript.php
+++ vendor/alextigaer/laravel-basic-project/src/Scripts/PostCreateProjectScript.php
@@ -2,11 +2,14 @@
 
 namespace LaravelBasicProject\Scripts;
 
+use Composer\Script\Event;
+
 class PostCreateProjectScript
 {
 
-    public static function editEnv(){
-        echo "PROVA";
+    public static function editEnv(Event $event){
+        echo "\n\n".$event->getName()."\n\n";
+        print_r($event->getArguments());
     }
 
 }
