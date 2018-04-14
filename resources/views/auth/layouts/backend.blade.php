35c9696523207177dd8715140af8a1cad0e77c57
Tiger <alvietri@gmail.com>
2018-04-14 18:46:40 +0200

    Updated jQuery version

--- resources/views/auth/layouts/backend.blade.php
+++ resources/views/auth/layouts/backend.blade.php
@@ -78,7 +78,7 @@
     </div>
 
     <!-- Scripts -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="http://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
     <script src="{{ asset('js/vendor/bootstrap/bootstrap.min.js') }}"></script>
     <script src="{{ asset('js/auth/backend.js') }}"></script>
